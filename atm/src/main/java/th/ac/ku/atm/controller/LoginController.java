f35aafb62bd3dd9b28f1f26ec673b5d905d26edc
malada leewalan <gam3542@hotmail.com>
2020-09-30 10:15:00 -0700

    add customer bankaccount feature

--- atm/src/main/java/th/ac/ku/atm/controller/LoginController.java
+++ atm/src/main/java/th/ac/ku/atm/controller/LoginController.java
@@ -7,15 +7,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import th.ac.ku.atm.model.Customer;
+import th.ac.ku.atm.service.BankAccountService;
 import th.ac.ku.atm.service.CustomerService;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
     private CustomerService customerService;
+    private BankAccountService bankAccountService;
 
-    public LoginController(CustomerService customerService) {
+
+    public LoginController(CustomerService customerService, BankAccountService bankAccountService) {
         this.customerService = customerService;
+        this.bankAccountService = bankAccountService;
     }
 
     @GetMapping
@@ -25,12 +29,18 @@ public class LoginController {
 
     @PostMapping
     public String login(@ModelAttribute Customer customer, Model model) {
-        Customer matchingCustomer = customerService.checkPin(customer);
-        if(matchingCustomer != null) {
-            model.addAttribute("greeting","Welcome "+ matchingCustomer.getName());
+        Customer storedCustomer = customerService.checkPin(customer);
+
+        if (storedCustomer != null) {
+            model.addAttribute("customertitle",
+                    storedCustomer.getName() + " Bank Accounts");
+            model.addAttribute("bankaccounts",
+                    bankAccountService.getCustomerBankAccount(customer.getId()));
+            return "customeraccount";
         } else {
-            model.addAttribute("greeting","Can't find customer");
+            model.addAttribute("greeting", "Can't find customer");
+            return "home";
         }
-        return "home";
+
     }
 }
