d413c191cffa46ae4e05e9e7c56cecd72412a09b
malada leewalan <gam3542@hotmail.com>
2020-09-30 13:11:57 -0700

    PUT : edit account(balance)

--- atm/src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ atm/src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -53,5 +53,21 @@ public class BankAccountService {
 //        return new ArrayList<>(this.bankAccountList);
 //    }
 
+    public BankAccount getBankAccount(int id) {
+        String url = "http://localhost:8091/api/bankaccount/" + id;
+
+        ResponseEntity<BankAccount> response =
+                restTemplate.getForEntity(url, BankAccount.class);
+
+        return response.getBody();
+    }
+
+    public void editBankAccount(BankAccount bankAccount) {
+        String url = "http://localhost:8091/api/bankaccount/" +
+                bankAccount.getId();
+        restTemplate.put(url, bankAccount);
+    }
+
+
 
 }
